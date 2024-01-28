1452823f60abb0fc65bd4bba1c9e4cca512dbe30
Debashree Chakraborty <90754321+dshree-21@users.noreply.github.com>
2024-01-28 12:23:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ================ typing animation =========== */
+var typed= new Typed(".typing",{
+    strings:["Final Year Graduate in CSE (B.Tech)","Software Developer","Web Developer","Morgan Stanley Intern"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
